011f5771fdeecf36d6274239056b559cacc35f9f
Fizza Usman <52672094+codes-by-kiwi@users.noreply.github.com>
2020-04-03 13:16:04 +0200

    Add files via upload

---
+++ in.py
@@ -0,0 +1,8 @@
+
+#The "in" operator
+
+name = "Laura"
+if name in ["Laura", "Riley," "Vicky","Julia", "Elena", "Sherry"]: #if "Laura" is in this list then the next line will be executed.
+    print("You are one of Fizza's friends")
+
+#The "in" operator could be used to check if a specified object exists within an iterable object container
